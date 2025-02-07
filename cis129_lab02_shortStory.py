dd473d883960fefa3d06c5cd7005ac3dff8c738b
JulianC25 <jcorbett2@mail.pima.edu>
2025-02-07 13:54:19 -0700

    Update cis129_lab02_shortStory.py
    Created a short story using print()

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('Hello, World!')
+#Author: Julian Corbett
+#My first Python lab in my CIS129 course 
+#Prints a simple short story 
+print("Today was the craziest day of my life, I won the lottery") 
+print("However, it was quickly ruined") 
+print("When I opened my eyes and saw the ceiling of my bedroom") 
